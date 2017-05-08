dd1b2c6f0520a36da07af2a541e1a26f850eecd3
fruit <fruit22@mail.ru>
2017-05-08 22:34:58 +0300

    start project

---
+++ src/main/java/com/jetBrains/StepikApplication.java
@@ -0,0 +1,12 @@
+package com.jetBrains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StepikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StepikApplication.class, args);
+	}
+}
