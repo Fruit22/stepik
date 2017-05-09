b586add4615972769314c7f0baa9e469e25e1ce1
fruit <fruit22@mail.ru>
2017-05-10 00:59:31 +0300

    performance test

--- src/test/java/com/jetBrains/StepikApplicationTests.java
+++ src/test/java/com/jetBrains/StepikApplicationTests.java
@@ -1,7 +1,9 @@
 package com.jetBrains;
 
+import com.jetBrains.dao.LessonDaoImpl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +11,22 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class StepikApplicationTests {
 
+	@Autowired
+	LessonDaoImpl lessonDao;
+
 	@Test
-	public void contextLoads() {
+	public void performanceTest() {
+		long start = System.nanoTime();
+		lessonDao.getSteps(1);
+		long end = System.nanoTime();
+		long time = end - start;
+		System.out.println("Time of the first request: " + time);
+
+		start = System.nanoTime();
+		lessonDao.getSteps(1);
+		end = System.nanoTime();
+		time = end - start;
+		System.out.println("Time of the second request: " + time);
 	}
 
 }
